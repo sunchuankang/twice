998ec29f8e87d2cef8d453e2fc5939f56751a516
sck <sckzen@gmail.com>
2025-05-05 13:32:53 +0800

    quzz

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,24 @@ import { useState } from "react";
 import { Button } from "antd";
 import "./App.css";
 import Theader from "./Theader";
+import Tsider from "./Tsider";
 function App() {
   const [count, setCount] = useState(0);
   return (
-    <div className="h-screen flex flex-col gap-4">
+    <div className="h-screen flex flex-col ">
       <Theader></Theader>
 
-      <div>{count}</div>
-      <div>
-        <Button onClick={() => setCount(count + 1)}>+1</Button>
+      <div className="flex h-full">
+        <div className=" w-30 bg-amber-900">
+          <Tsider></Tsider>
+        </div>
+
+        <div className="text-center m-auto ">
+          <div>{count}</div>
+          <div>
+            <Button onClick={() => setCount(count + 1)}>+1</Button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Tsider.jsx
+++ src/Tsider.jsx
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  LaptopOutlined,
-  NotificationOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
-import { Breadcrumb, Layout, Menu, theme } from "antd";
 
 export default function Tsider() {
-  return <div></div>;
+  return <div>menu</div>;
 }
